0a7f06edd8c44fbde6ed7a1c5d367076d1215d97
Anil <durananil@outlook.com>
2024-04-30 16:43:03 +0300

    Add pagination to query resolver

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -9,28 +9,135 @@ import User from '../models/user'
 const Query = {
     async users(parent, args, contextValue, info) {
         authenticate(contextValue.token)
-        const users = await User.find()
-        return users
+
+        let query = {}
+
+        if (args.cursor) {
+            query = {
+                _id: { $gt: args.cursor }
+            }
+        }
+
+
+        const users = await User.find(query).limit(args.limit + 1).sort({ _id: 1 }).exec()
+
+        const hasNextPage = users.length > args.limit
+        const edges = hasNextPage ? users.slice(0, -1) : users
+        const endCursor = edges[edges.length - 1]?._id
+        
+        return {
+            edges,
+            pageInfo: {
+                hasNextPage,
+                endCursor
+            }
+        }
+
     },
     async posts(parent, args, contextValue, info) {
         authenticate(contextValue.token)
-        const posts = await Post.find()
-        return posts
+
+        let query = {}
+
+        if (args.cursor) {
+            query = {
+                _id: { $gt: args.cursor }
+            }
+        }
+
+
+        const posts = await Post.find(query).limit(args.limit + 1).sort({ _id: 1 }).exec()
+
+        const hasNextPage = posts.length > args.limit
+        const edges = hasNextPage ? posts.slice(0, -1) : posts
+        const endCursor = edges[edges.length - 1]?._id
+        
+        return {
+            edges,
+            pageInfo: {
+                hasNextPage,
+                endCursor
+            }
+        }
     },
     async categories(parent, args, contextValue, info) {
         authenticate(contextValue.token)
-        const categories = await Category.find()
-        return categories        
+        
+        let query = {}
+
+        if (args.cursor) {
+            query = {
+                _id: { $gt: args.cursor }
+            }
+        }
+
+        const categories = await Category.find(query).limit(args.limit + 1).sort({ _id: 1 }).exec()
+        
+        const hasNextPage = categories.length > args.limit
+        const edges = hasNextPage ? categories.slice(0, -1) : categories
+        const endCursor = edges[edges.length - 1]?._id
+
+        return {
+            edges,
+            pageInfo: {
+                hasNextPage,
+                endCursor
+            }
+        }        
     },
     async comments(parent, args, contextValue, info) {
         authenticate(contextValue.token)
-        const comments = await Comment.find()
-        return comments
+
+        let query = {}
+
+        if (args.cursor) {
+            query = {
+                _id: { $gt: args.cursor }
+            }
+        }
+
+
+        const comments = await Comment.find(query).limit(args.limit + 1).sort({ _id: 1 }).exec()
+
+        const hasNextPage = comments.length > args.limit
+        const edges = hasNextPage ? comments.slice(0, -1) : comments
+        const endCursor = edges[edges.length - 1]?._id
+
+
+        return {
+            edges,
+            pageInfo: {
+                hasNextPage,
+                endCursor
+            }
+        }
     },
     async readingLists(parent, args, contextValue, info) {
         authenticate(contextValue.token)
-        const readingLists = await ReadingList.find()
-        return readingLists
+
+        let query = {}
+
+        if (args.cursor) {
+            query = {
+                _id: { $gt: args.cursor }
+            }
+        }
+
+
+        const readingLists = await ReadingList.find(query).limit(args.limit + 1).sort({ _id: 1 }).exec()
+
+        const hasNextPage = readingLists.length > args.limit
+        const edges = hasNextPage ? readingLists.slice(0, -1) : readingLists
+        const endCursor = edges[edges.length - 1]?._id
+
+
+        return {
+            edges,
+            pageInfo: {
+                hasNextPage,
+                endCursor
+            }
+        }
     },
     async me(parent, args, contextValue, info) {
         const authenticatedUser = authenticate(contextValue.token)
